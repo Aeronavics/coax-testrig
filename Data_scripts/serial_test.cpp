bab26a8862bd08bc2de30d9b30ceae1a521ea0e3
unknown <oliver.clements@aeronavics.com>
2022-11-25 15:25:22 +1300

    added test_file

---
+++ Data_scripts/serial_test.cpp
@@ -0,0 +1,45 @@
+// This is an Arduino file
+
+// Program that simply writes to serial monitor for testing
+String datalabel1 = "Power1";
+String datalabel2 = "Speed1";
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+bool done = false;
+
+void loop() {
+
+  while(!done) 
+  {
+    Serial.print(datalabel1 + ",");
+    Serial.println(datalabel1);
+    break;
+  }
+
+  if(done == false)
+  {
+    printer();
+    done = true;
+  }
+
+}
+
+
+void printer(void) 
+{
+  int i = 0;
+  while(i < 20) 
+  {
+    int hold = i;
+    Serial.print(hold, DEC);
+    Serial.print(",");
+    Serial.print(hold + 10, DEC);
+    Serial.print(",");
+    Serial.println(hold * 3.145, DEC);
+    i++;
+  }
+}
