dccc17d27b012562a53564be5856265fdcf1a303
unknown <oliver.clements@aeronavics.com>
2022-11-28 13:52:46 +1300

    Python now starts test run

--- Test Rig Program/Test Rig Program.ino
+++ Test Rig Program/Test Rig Program.ino
@@ -1,3 +1,10 @@
+/*
+  Code that runs the test and puts data into csv format for python to read
+  // Automatically changes motor speeds
+
+*/
+
+
 #include <Servo.h>
 #include "HX711.h"
 #include "ACS758.h"
@@ -46,33 +53,49 @@ void setup() {
   bottom_esc.writeMicroseconds(1000);
 
   Serial.begin(9600);
-  Serial.println("Enter 1 to start");
+  Serial.println("Time:");
 }
+
+void printer(int speed) {
+  for(int reps = 0; reps < 1; reps++) {
+    Serial.print(speed);
+    Serial.print(",");
+    Serial.print(top_motor.voltage());
+    Serial.print(",");
+    Serial.print(bottom_motor.voltage());
+    Serial.print(",");
+    Serial.print(top_motor.current());
+    Serial.print(",");
+    Serial.print(bottom_motor.current());
+    Serial.print(",");  
+    Serial.println(loadcell.get_units(10));
+  }
+}
+
+bool done = false;
 
 void loop() {
   if (Serial.available() > 0) {
     if (Serial.read() == '1') {
-      Serial.println("Motor PWM, Top Voltage (V), Bottom Voltage (V), Top Current (A), Bottom Current (A), Thrust (kg)");
-      for (int speed = SPEED_MIN; speed <= SPEED_MAX; speed += SPEED_INC) {
-        top_esc.writeMicroseconds(speed);
-        bottom_esc.writeMicroseconds(speed);
-        delay(3000);
-        Serial.print(speed);
-        Serial.print(", ");
-        Serial.print(top_motor.voltage());
-        Serial.print(", ");
-        Serial.print(bottom_motor.voltage());
-        Serial.print(", ");
-        Serial.print(top_motor.current());
-        Serial.print(", ");
-        Serial.print(bottom_motor.current());
-        Serial.print(", ");
-        Serial.print(loadcell.get_units(10));
-        Serial.println(",");
+      Serial.println("Turing Power On!");
+      while(!done) {
+        Serial.println("Motor PWM, Top Voltage (V), Bottom Voltage (V), Top Current (A), Bottom Current (A), Thrust (kg)");
+        for (int speed = SPEED_MIN; speed <= SPEED_MAX; speed += SPEED_INC) {
+          top_esc.writeMicroseconds(speed);
+          bottom_esc.writeMicroseconds(speed);
+          delay(1000);
+          printer(speed);
+        }
+        top_esc.writeMicroseconds(1000);
+        bottom_esc.writeMicroseconds(1000);
+        Serial.println("Finished");
+        done = true;
       }
-      top_esc.writeMicroseconds(1000);
-      bottom_esc.writeMicroseconds(1000);
-      Serial.println("Finished");
+
+      Serial.flush();
     }
   }
+
+  delay(2000);
+  Serial.println("Waiting for Authorization");
 }
