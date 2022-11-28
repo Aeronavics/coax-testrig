85f1c512995ab1f00116d3402a787fa30340b51e
unknown <oliver.clements@aeronavics.com>
2022-11-29 11:29:22 +1300

    added common funcs

---
+++ Data_scripts/common_funcs.py
@@ -0,0 +1,28 @@
+# ======================================================================
+# AUTHOR        : Oliver Clements
+# CREATE DATE   : 29/11/22
+# PURPOSE       : File with common functions
+# ======================================================================
+
+from colorama import Fore
+
+def print_list(list):
+    """Prints elements in list"""
+    
+    for item in list:
+        print(Fore.RESET + f"{item}")
+        
+
+def list_check(user_input, list_used, data_type):
+    """Returns the prop size in list"""
+    try:
+        if data_type(user_input) in list_used:
+            return True
+        else:
+            print(Fore.RED + f"\nThat is not one of the options")
+            print("Try again\n")
+            return False
+    except:
+        print(Fore.RED + f"\nThat is not a {str(data_type)} data type")
+        print("Try again\n")
+        return False
