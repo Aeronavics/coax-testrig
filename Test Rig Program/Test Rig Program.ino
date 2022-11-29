33dcd0374399b6d0390ab95b99a9f70212b5b291
unknown <oliver.clements@aeronavics.com>
2022-11-30 10:11:47 +1300

    added saftey features

--- Test Rig Program/Test Rig Program.ino
+++ Test Rig Program/Test Rig Program.ino
@@ -36,17 +36,22 @@
 #define CURRENT_OFFSET_TOP 0.165
 #define CURRENT_OFFSET_BOTTOM 0
 
+#define ONE_THOUSAND 1000
+
 ACS758 top_motor(CIN_TOP, VIN_TOP, CURRENT_RATIO_TOP, VOLTAGE_RATIO_TOP, CURRENT_OFFSET_TOP);
 ACS758 bottom_motor(CIN_BOTTOM, VIN_BOTTOM, CURRENT_RATIO_BOTTOM, VOLTAGE_RATIO_BOTTOM, CURRENT_OFFSET_BOTTOM);
 Servo top_esc;
 Servo bottom_esc;
 HX711 loadcell;
 
+bool SAFE = true;
+
 void setup() {
 
   if(SPEED_MAX > 2000) {
     Serial.println("MAX SPEED IS TOO HIGH");
     Serial.println("Aborting");
+    SAFE = false;
     abort();
   }
   //Initialise load cell
@@ -57,8 +62,8 @@ void setup() {
   //Initialise ESCs
   top_esc.attach(TOP_ESC);
   bottom_esc.attach(BOTTOM_ESC);
-  top_esc.writeMicroseconds(1000);
-  bottom_esc.writeMicroseconds(1000);
+  top_esc.writeMicroseconds(ONE_THOUSAND);
+  bottom_esc.writeMicroseconds(ONE_THOUSAND);
 
   Serial.begin(9600);
   Serial.println("Time:");
@@ -85,24 +90,32 @@ void printer(int speed) {
 
 bool done = false;
 
+void motor_speed(int speed) {
+  top_esc.writeMicroseconds(speed);
+  bottom_esc.writeMicroseconds(speed);
+  delay(400);
+  printer(speed);
+}
+
 void loop() {
   // Waits for '1' to be sent from python code
   if (Serial.available() > 0) {
-    delay(1000);
-    if (Serial.read() == '1') {
+    delay(ONE_THOUSAND);
+    if (Serial.read() == '1' && SAFE == true) {
       Serial.println("Turing Power On!");
       delay(3000);
 
       while(!done) {
         Serial.println("Motor PWM, Top Voltage (V), Bottom Voltage (V), Top Current (A), Bottom Current (A), Thrust (kg)");
         for (int speed = SPEED_MIN; speed <= SPEED_MAX; speed += SPEED_INC) { // Toggles ESC PWM
-          top_esc.writeMicroseconds(speed);
-          bottom_esc.writeMicroseconds(speed);
-          delay(400);
-          printer(speed);
-        }
-        top_esc.writeMicroseconds(1000);
-        bottom_esc.writeMicroseconds(1000);
+          if(speed < SPEED_MAX) {
+            motor_speed(speed);
+          } else {
+            motor_speed(ONE_THOUSAND);
+          }
+          
+        } 
+        motor_speed(ONE_THOUSAND);
         Serial.println("Finished");
         done = true;
       }
@@ -111,7 +124,7 @@ void loop() {
     }
   }
   else {  // Wait time so python can communicate w Arduino
-    delay(1000);
+    delay(ONE_THOUSAND);
     Serial.println("Waiting...");
   }
 
