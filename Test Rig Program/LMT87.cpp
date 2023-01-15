19e5a4402cfa8c7b114d8436651d6a5d79bbbc13
OliverClements189 <oliver.clements@aeronavics.com>
2023-01-16 12:35:53 +1300

    added comments

--- Test Rig Program/LMT87.cpp
+++ Test Rig Program/LMT87.cpp
@@ -1,3 +1,10 @@
+//=========================================================================
+//  Author: Reuben Campbell
+//  Date Created: 10/01/2023
+//  Description: Returns infomation about the temperature value from the 
+//               LMT87 sensors
+//=========================================================================
+
 #include "LMT87.h"
 
 #define ADC_SCALE 2.76 / 1023.0
