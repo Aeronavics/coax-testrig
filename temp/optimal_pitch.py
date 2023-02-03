362721603e5499335c74d9d53ad328a62206bb77
OliverClements189 <oliver.clements@aeronavics.com>
2023-02-03 14:14:22 +1300

    tweaks

--- temp/optimal_pitch.py
+++ temp/optimal_pitch.py
@@ -9,19 +9,22 @@ from pitch_assist import Prop, Motor, Velocity, rpm_to_theta
 # Constants (may need to be altered)
 AIR_DENSITY = 1.21
 MOTOR_EFFICIENCY = 0.8
-LOSES = 0.82
+LOSES = 0.83    # Guessed value. About 22% less thrust is produced than the ideal equation so this is a value somewhere in that range
+                # Works well with
 
 
 # PROP TO CHOOSE FROM
 F18 = Prop(18.3, 6.2, 0.04)
-FOLD = Prop(18.2, 5.9, 0.04)
+FOLD18 = Prop(18.2, 5.9, 0.04)
+FOLD16 = Prop(16.2, 5.3, 0.04)
+FOLD15 = Prop(15.2, 5, 0.04)
 
 # ======================================
 # EDIT THESE VALUES!!!!
-TOP_MOTOR = Motor(140, 0.51, 48.3, 2.55)
-BOT_MOTOR = Motor(160, 0.51, 48.3, 4)
-TOP_PROP = FOLD
-BACK_PROP = FOLD
+TOP_MOTOR = Motor(160, 0.51, 48.3, 3.8)
+BOT_MOTOR = Motor(220, 0.51, 48.3, 5)
+TOP_PROP = F18
+BACK_PROP = FOLD15
 # ======================================
 
 v1 = sp.Symbol('v1', real=True)        
@@ -56,7 +59,9 @@ def give_v1(v0_star: Velocity, back_prop: Prop, back_motor: Motor) -> Velocity:
     power = back_motor.give_power()
     Acs = back_prop.give_Acs()
     solution = sp.solve((v1 ** 3) - (v0_star.vy * v1 ** 2) - (power * MOTOR_EFFICIENCY / (2 * Acs * AIR_DENSITY)), v1)
-    v1_ = max(solution)
+    v1_ = max(solution) 
+    loses = (v1_ - v0_star.vy) * ((1 - LOSES) /  3 ) # this 3 just seemed to work better than a 2. Possible error source
+    v1_ = v1_ - loses
     
     return Velocity(0, v1_)
 
@@ -74,11 +79,18 @@ def give_v_rel(prop: Prop, motor: Motor) -> Velocity:
     
     return Velocity(v_rel, 0)
 
-def pitch_to_angle_ratio(prop: Prop) -> float:
-    angle = prop.angle()
-    return angle / prop.pitch
+# def pitch_to_angle_ratio(prop: Prop) -> float:
+#     angle = prop.angle()
+#     return angle / prop.pitch
 
-def optimal_angle(v0_total: Velocity, v1_total: Velocity, bottom_prop: Prop) -> float:
+def angle_comp(prop: Prop, v_total: Velocity) -> float:
+    
+    v_angle = v_total.give_angle()
+    prop_angle = prop.g_angle()
+    return v_angle - prop_angle
+
+
+def optimal_angle(v0_total: Velocity, v1_total: Velocity, free_v1_total: Velocity, bottom_prop: Prop) -> float:
     """ Finds the ratio to pitch in inches to an angle. 
         Assumes linear relationship so only can be used for small angles
 
@@ -92,10 +104,20 @@ def optimal_angle(v0_total: Velocity, v1_total: Velocity, bottom_prop: Prop) -> 
     
     v0_angle = v0_total.give_angle()
     v1_angle = v1_total.give_angle()
-    diff_angle = v1_angle - v0_angle
-    ratio = pitch_to_angle_ratio(bottom_prop)
+    free_v1_angle = free_v1_total.give_angle()
+    back_prop_angle = bottom_prop.g_angle()
     
-    print(f"Angle: {diff_angle}")
+    print(v0_angle)
+    print(v1_angle)
+    print(free_v1_angle)
+    print(back_prop_angle)
+    
+    diff_v_angle = v1_angle - v0_angle
+    diff_pv_angle = back_prop_angle - free_v1_angle
+    
+    opt_angle = diff_v_angle - diff_pv_angle
+    
+    print(f"Angle: {opt_angle}")
     
 
 
@@ -109,23 +131,42 @@ def main(top_prop: Prop, back_prop: Prop, top_motor: Motor, back_motor: Motor):
         back_motor (Motor): The back motor to use
     """
     
+    # Calculates normal velocity's
     v0 = give_v0(top_prop, top_motor)
     v0_star = give_v0_star(v0)
     v1 = give_v1(v0_star, back_prop, back_motor)
+    print(v0_star)
+    print(v1)
+    
+    # In plane velocities
     v_swirl = give_v_swirl(top_prop, top_motor)
     v0_rel = give_v_rel(top_prop, top_motor)
     v1_rel = give_v_rel(back_prop, back_motor)
     
+    # Total velocities
     v0_total = Velocity.add_vectors([v0, v0_rel])
     v1_total = Velocity.add_vectors([v1, v_swirl, v1_rel])
+    
+    # Velocity if bottom prop had a free stream velocity of 0
+    free_v1_bot = give_v0(back_prop, back_motor)
+    free_v1_total = Velocity.add_vectors([free_v1_bot, v1_rel])
+    
+    optimal_angle(v0_total, v1_total, free_v1_total, back_prop)
+    
     v0_mag = v0_total.give_magnitude()
     v1_mag = v1_total.give_magnitude()
     v0_angle = v0_total.give_angle()
     v1_angle = v1_total.give_angle()
     
-    print(f"v0 mag: {v0_mag}, v0 angle: {v0_angle}")
-    print(f"v1 mag: {v1_mag}, v1 angle: {v1_angle}")
-    print(top_prop.angle())
+    # print(f"v0 mag: {v0_mag}, v0 angle: {v0_angle}")
+    # print(f"v1 mag: {v1_mag}, v1 angle: {v1_angle}")
+    
+    # print(back_prop.g_angle())
 
+    # diff_angle = v1_angle - v0_angle
+    # # angle_comp(TOP_PROP, v0_total)
+    # print(diff_angle)
+
+    
 main(TOP_PROP, BACK_PROP, TOP_MOTOR, BOT_MOTOR)
 
